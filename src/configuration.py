Bug fixed in distance integral calculation. Bug fixed in data types. Added paths for plotting and chromosomes.

src/configuration.py
@@ -34,7 +34,7 @@ class param:
         self.id_tick = id_tick
         if id_tick >= self.num_ticks or id_tick < 0:
             raise IndexError
-        if isinstance(self.value_be, (np.floating, float, int)) and isinstance(self.value_en, (np.floating, float, int)):
+        if isinstance(self.value_be, (np.floating, float, int, long)) and isinstance(self.value_en, (np.floating, float, int, long)):
             if self.num_ticks > 1: self.value = (self.value_en - self.value_be) * id_tick / (self.num_ticks - 1) + self.value_be
             else: self.value = value_be
         elif self.value_list and hasattr(self.value_list, '__getitem__'):

src/transform_data.py
@@ -22,7 +22,8 @@ def calc_edge_weight(feature_i, feature_j, kde, p, I, min_x, max_x):
     
     q = kde(np.array([feature_i, feature_j])) # .reshape(1, -1)
     pos = np.searchsorted(p, q)
-    w = (I[pos] + q * (p.size - pos)) / p.size
+    #w = (I[pos] + q * (p.size - pos)) / p.size
+    w = (I[-1] - I[pos]) / p.size
 #    print p
 #    w = np.random.rand()
 #    stop = timeit.default_timer()
@@ -291,10 +292,11 @@ def make_genes_edge(X_prob_i, X_prob_j, X_i, X_j, thresholds_p):
     points = kde.resample(num_points)
     pr = np.array(kde(points))
     pr.sort()
-    I = np.concatenate([np.cumsum(pr), [1]])
+    #I = np.concatenate([np.cumsum(pr), [1]])
     G = np.zeros((len(thresholds_p), X_i.shape[0]), dtype=np.bool)
     for i, thr_p in enumerate(thresholds_p):
-        ind = np.flatnonzero(I >= thr_p)[0]
+        #ind = np.flatnonzero(I >= thr_p)[0]
+        ind = int(thr_p * num_points)
         if ind < pr.size:
             q = pr[ind]
             G[i] = p < q