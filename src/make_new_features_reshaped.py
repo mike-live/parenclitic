ac45f1f5965e8ecfd2b994b470a220333d868a28
mike_live <mike_live@mail.ru>
2019-01-31 14:34:47 +0300

    Updated configurations. Added pickle compression

--- src/make_new_features_reshaped.py
+++ src/make_new_features_reshaped.py
@@ -1,9 +1,9 @@
 from threading import Event, Lock, Semaphore
 import timeit
 import numpy as np
-from make_graphs_parts import *
+from transform_data import *
 import sys
-from auxillary import *
+from infrastructure.auxillary import *
 import time
 import fcntl
 import os
@@ -11,19 +11,24 @@ import os
 #from ages_config import config
 #from load_data_age import load_data_age
 
-from load_data_mongoloids import load_data_mongoloids
-from mongoloids_config import config
+#from load_data_mongoloids import load_data_mongoloids
+#from mongoloids_config import config
+
+from configurations.load_data_down_GSE63347 import load_data_down_GSE63347
+from configurations.config_down_GSE63347 import config
 
 def calc_new_feautures(x, g, i):
     #disable_print()
     g = np.unpackbits(g, axis = 1)[:, :x.size]
-    res = (i, get_degrees(g), parenclitic_transform(x, G = g))
+    res = (i, parenclitic_transform(x, G = g))
     #enable_print()
     return res
 
 def make_new_features(X):
     i = config.params["id_sample"].value
-    thr_p = config.params["thr_p"].value
+    thr_p = -1
+    if "thr_p" in config.params:
+        thr_p = config.params["thr_p"].value
 
     print 'Make new features for', i, '/', X.shape[0], 'and thr_p', thr_p
     sys.stdout.flush()
@@ -42,14 +47,14 @@ def make_new_features(X):
     
 
 if __name__ == '__main__':
-    
-
     config.params["id_part"].manual_ticks = True
 
     config.upd_ticks()
 
     i = config.params["id_sample"].value
-    thr_p = config.params["thr_p"].value
+    thr_p = -1
+    if "thr_p" in config.params:
+        thr_p = config.params["thr_p"].value
 
     print 'Start', i, '/', config.params['num_samples'].value, 'and thr_p', thr_p
 
@@ -61,12 +66,11 @@ if __name__ == '__main__':
     if os.path.exists(degrees_path) and os.path.exists(parenclitic_path):
         sys.exit(0)
 
-
-    X, y, _, genes_names = load_data_mongoloids()    
+    X, y, _, genes_names = load_data_down_GSE63347()    
     #X, y, _, genes_names = load_data_age()
     config.params["num_genes"].value = min(genes_names.size, config.params["num_genes"].value)
 
-    id_sample, degrees, parenclitic = make_new_features(X)
+    id_sample, parenclitic = make_new_features(X)
 
     config.save_params(include_set = config.params_sets["degrees"])
     
@@ -81,5 +85,6 @@ if __name__ == '__main__':
         fcntl.lockf(f,fcntl.LOCK_UN)
         os.close(f)
     '''
-    np.savetxt(degrees_path, degrees, delimiter = '\t', fmt = '%6d')
-    np.savetxt(parenclitic_path, parenclitic, delimiter = '\t')
+    #np.savetxt(degrees_path, degrees, delimiter = '\t', fmt = '%6d')
+    #np.savetxt(parenclitic_path, parenclitic, delimiter = '\t')
+    parenclitic.to_pickle(config.ofname(["parenclitic"], ext = ".pkl", include_set = config.params_sets["parenclitic_sample"]), compression = "gzip")
