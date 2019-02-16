65ff9c48f055feeed61f97d9bd36bc3777be9dee
Krivonosov Mikhail <mike_live@mail.ru>
2019-02-16 11:28:41 +0300

    Added parenclitic txt loading.

--- src/load_parenclitics.py
+++ src/load_parenclitics.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import os
 def load_parenclitics(config, by_sample = False):
     if by_sample:
         parenclitics = None
-        for id_sample in config.params["id_sample"]:
-            parenclitic = pd.read_pickle(config.ofname(["parenclitic"], ext = ".pkl", include_set = config.params_sets["parenclitic_sample"]), compression = "gzip")
+        for id_sample in config.params["id_sample"]:    
+            parenclitic = None
+            pkl_file = config.ofname(["parenclitic"], ext = ".pkl", include_set = config.params_sets["parenclitic_sample"])
+            if os.path.exists(pkl_file):
+                parenclitic = pd.read_pickle(pkl_file, compression = "gzip")
+            
+            txt_file = config.ofname(["parenclitic"], ext = ".txt", include_set = config.params_sets["parenclitic_sample"])
+            if os.path.exists(txt_file):
+                parenclitic = pd.read_csv(txt_file, sep=" ", header=None).T
+                
             if parenclitics is None:
                 parenclitics = parenclitic
             else:
