3bcdc88aa1641ab66449eb3f13a862da86cec414
Mikhail Krivonosov <mike_live@mail.ru>
2020-03-10 00:29:44 +0300

    Added readme to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='parenclitic',
-      version='0.1.6',
+      version='0.1.7',
       description='Parenclitic approach with kernels inside',
       long_description=long_description,
       long_description_content_type="text/markdown",
